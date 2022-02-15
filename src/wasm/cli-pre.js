f50b6eceb9b0c5d9c2b10878b9fbf13bf571f0f9
Danielle Church <dani.church@gmail.com>
2022-02-15 05:08:56 +0100

    Remove workarounds for old Emscripten

--- src/wasm/cli-pre.js
+++ src/wasm/cli-pre.js
@@ -1,32 +1,9 @@
 Error.stackTraceLimit = 50;
 var isInitialized = false;
 var inStackTrace = false;
-Module = {
+Object.assign(Module, {
     preInit: function() {
         ENV = process.env;
-        var orig_exit = exit;
-        exit = function(status) {return orig_exit(status, true);}; // avoid the "main thread called exit" message when multithreaded
-        NODEFS.flagsForNodeMap[65536] = fs.constants.O_DIRECTORY;
-        NODERAWFS.readdir = function() {return fs.readdirSync.apply(void 0, arguments)};
-        FS.readdir = _wrapNodeError(NODERAWFS.readdir);
-        SYSCALLS.getStreamFromFD = function(fd) {
-            var stream = FS.getStream(fd);
-            if (!stream) throw new FS.ErrnoError(8);
-            if ((stream.flags & 65536) && !stream.node && stream.path) {
-                const lookup = FS.lookupPath(stream.path);
-                if (!lookup) throw new FS.ErrnoError(8);
-                stream.node = Object.assign({}, lookup.node, {
-                    path: stream.path,
-                });
-            }
-            return stream;
-        };
-        FS.lookupNode = function(parent, name) {
-            if (parent.path) {
-                return FS.lookupPath(NODEJS_PATH.join(parent.path, name));
-            }
-            return VFS.lookupNode(parent, name);
-        };
     },
     onRuntimeInitialized: function() {
         isInitialized = true;
@@ -49,4 +26,4 @@ Module = {
             inStackTrace = false;
         }
     },
-};
+});
